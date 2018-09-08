7a79a471647795058eaf1e1368fd28dabfaee41d
PK <kent.pm@gmail.com>
2018-09-07 20:49:39 -0700

    building out app

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-console.log("hello world of node");
 
-require('./app/index')
-// Load HTTP module
-var http = require("http");
+var express=require('express');
+var app=express();
+console.log(__dirname);
+app.use(express.static(__dirname+'/assets'));
 
-// Create HTTP server and listen on port 8000 for requests
-http.createServer(function(request, response) {
 
-   // Set the response HTTP header with HTTP status and Content type
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body "Hello World"
-   response.end('Hello World im gonna wreck you\n');
-}).listen(8000);
+app.listen(8000, function(){
+	console.log("listen on port 8000")
+});
 
-// Print URL for accessing server
-console.log('Server running at http://127.0.0.1:8000/');
+console.log("initiating killbot 10,000");
